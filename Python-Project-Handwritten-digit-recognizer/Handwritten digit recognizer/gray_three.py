cd203fe6eceb681af544af2a5ec5d420684ddebd
Kara230 <66848376+Kara230@users.noreply.github.com>
2021-05-06 19:54:57 -0400

    Update gray_three.py

--- Python-Project-Handwritten-digit-recognizer/Handwritten digit recognizer/gray_three.py
+++ Python-Project-Handwritten-digit-recognizer/Handwritten digit recognizer/gray_three.py
@@ -30,27 +30,20 @@ def prepare(path):
 '''
 
 
-#Works kinda prints one every time; makes sense since we resized the image twice and it looks like one 
+# Formatting image for model 
 def prepare(path):      
     try:
         IMG_SIZE = 28
         img = Image.open(path)
         img = ImageOps.grayscale(img)
-        print(img.size)
+        print('Orignal Image:', img.size)
         resized_img = img.resize((IMG_SIZE, IMG_SIZE))
-        resized_img.save('8_resized.jpg')        # save the image as something else, not the name of the original photo
+        resized_img.save('8_resized.jpg', quality=95)                               # save the image as something else, not the name of the original photo
         resized_img.show()
-        print(resized_img.size)
+        print('Resized Grayscale Image:', resized_img.size)
         img_array = cv2.imread('8_resized.jpg')
-        #plt.imshow(img_array, cmap = plt.cm.gray)
-        #plt.show()
-        #new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-        #plt.imshow(new_array, cmap = plt.cm.gray)
-        #plt.show()
         new_array = img_array.astype('float32')
         new_array = new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        #new_array = new_array / 255.0
-        #print(new_array)
         return new_array
     except Exception as e:
         print(str(e))
@@ -73,7 +66,6 @@ def prepare(path):
     except Exception as e:
         print(str(e))
 
-        '''
 model = tf.keras.models.load_model('gray_model.h5', compile = True)
 #model.compile(loss="sparse_categorical_crossentropy", optimizer= "adam", metrics=['accuracy'])
 #os.system("cd jpg_photos")
@@ -84,7 +76,15 @@ prediction = model.predict([prepare('8.jpg')])      #always pass a list
 print(CATEGORIES[int(prediction[0][0])])
 
 #classes = np.argmax(prediction, axis = 1)
-#print(classes)
+#print(classes)        
+        
+'''
+model = tf.keras.models.load_model('gray_model.h5', compile = True)
+os.chdir("jpg_photos")
+os.system("cd")
+prediction = model.predict([prepare('8.jpg')])                           #always pass a list
+print(CATEGORIES[int(prediction[0][0])])
+
 
 '''
 model = tf.keras.models.load_model('new_model.h5')
