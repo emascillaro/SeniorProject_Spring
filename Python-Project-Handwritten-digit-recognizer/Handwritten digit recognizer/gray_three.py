8f3a4abac3a0a65b361fcf3092103c0dd45f79a7
Kara230 <66848376+Kara230@users.noreply.github.com>
2021-05-26 10:57:06 -0400

    Update gray_three.py

--- Python-Project-Handwritten-digit-recognizer/Handwritten digit recognizer/gray_three.py
+++ Python-Project-Handwritten-digit-recognizer/Handwritten digit recognizer/gray_three.py
@@ -7,38 +7,68 @@ import os
 from keras.preprocessing import image
 from PIL import Image, ImageOps, ImageChops
 
-CATEGORIES = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
-PATH = r"jpg_photos"
+import bound_box
 
-'''
-# (SECOND BEST PREPARE METHOD TO USE)
+CATEGORIES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
+
+# Calling bound_box function 
+bound_box.box()
+prepared_img_list = []
+final_predict_list = []
+
+# (BEST PREPARE METHOD TO USE)
 # Formatting image for model 
 def prepare(path):      
     try:
         IMG_SIZE = 28
-        img = Image.open(path)
-        img = ImageOps.grayscale(img)
-      # img = ImageChops.invert(img)
-        print('Orignal Image:', img.size)
-        resized_img = img.resize((IMG_SIZE, IMG_SIZE))
-        resized_img.save('66_resized.jpg', quality=95)                             # save the image as something else, not the name of the original photo
-        resized_img.show()
-        print('Resized Grayscale Image:', resized_img.size)
-        img_array = cv2.imread('66_resized.jpg')
-        new_array = img_array.astype('float32')
+        img_array = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+        print('Orignal Image Array Shape:', img_array.shape)
+        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
+        new_array = new_array.astype('float32')
         new_array = new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
+        print('Resized Grayscale Image Array for Model:', new_array.shape)
         return new_array
     except Exception as e:
         print(str(e))
 
-
+# Compile model
 model = tf.keras.models.load_model('gray_model.h5', compile = True)
-os.chdir("jpg_photos")
-os.system("cd")
-prediction = model.predict([prepare('66.jpg')])                           #always pass a list
-print(CATEGORIES[int(prediction[0][0])])
+
+# Switch directory to folder with boxed images from box()
+directory = r'final_images'
+
+# Iterate through images in folder and convert to arrays using prepare()
+# Add image arrays to empty list
+count = 1
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f) and filename.endswith('.jpg'): 
+        print("Image #", count)
+        count += 1
+        prepared_image = prepare(f)
+        prepared_img_list.append(prepared_image)
+    
+
+# Iterate through image array list and predict each image 
+# Add image prediction to another empty list 
+for i in prepared_img_list:
+    prediction = model.predict(i) 
+    pred_name = CATEGORIES[np.argmax(prediction)]
+    final_predict_list.append(pred_name)
+    #print("Model's Prediction:", pred_name)
+
+# Convert String List to Integer List 
+final_predict_list = [int(i) for i in final_predict_list]
+print(final_predict_list)
+
+# Remove ALL image files from folder 
+for filename in os.listdir(directory):
+   os.remove(os.path.join(directory, filename))
 
 '''
+CATEGORIES = ["Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine"]
+PATH = r"final_images"
+
 # (BEST PREPARE METHOD TO USE)
 # Formatting image for model 
 def prepare(path):      
@@ -56,83 +86,12 @@ def prepare(path):
 
 
 model = tf.keras.models.load_model('gray_model.h5', compile = True)
-os.chdir("jpg_photos")
+os.chdir("final_images")
 os.system("cd")
-prediction = model.predict([prepare('66.jpg')])                           #always pass a list
+prediction = model.predict([prepare('ROI_2.jpg')])                           #always pass a list
 # print(np.argmax(prediction))
 pred_name = CATEGORIES[np.argmax(prediction)]
-print("Model's Prediction:", pred_name)
-
-# Place this code before speciying prediction and after os.system
-'''
-img = Image.open('50.jpg')
-inv_img = ImageChops.invert(img)
-inv_img.save('inv_50.jpg')
-inv_img.show()
-'''
-
-
-# OLDER PREPARE METHODS AND CODE
+print("Model's Prediction:", pred_name) 
 
 '''
-def prepare(path):      
-    try:
-        IMG_SIZE = 28
-        img = Image.open(path)
-        img = ImageOps.grayscale(img)
-        img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-        img.save('8_gray.jpg')
-        img.show()
-        print(img.size)
-        img_array = cv2.imread('8_gray.jpg')
-        new_array = img_array.astype('float32')
-        new_array = new_array.reshape(-1, IMG_SIZE, IMG_SIZE, 1)
-        #new_array = new_array / 255.0
-        #print(new_array)
-        return new_array
-    except Exception as e:
-        print(str(e))
 
-
-
-def prepare(path):
-    try: 
-        IMG_SIZE = 28
-       # path = os.path.join(path, img)
-        img_array = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
-       # plt.imshow(img_array, cmap = plt.cm.gray)
-        #plt.show()
-        new_array = cv2.resize(img_array, (IMG_SIZE, IMG_SIZE))
-        plt.imshow(new_array, cmap = plt.cm.gray)
-        plt.show()
-        new_array = new_array.astype('float32')
-        new_array = new_array.reshape(1, IMG_SIZE, IMG_SIZE, 1)
-        return new_array
-    except Exception as e:
-        print(str(e))
-
-model = tf.keras.models.load_model('gray_model.h5', compile = True)
-#model.compile(loss="sparse_categorical_crossentropy", optimizer= "adam", metrics=['accuracy'])
-#os.system("cd jpg_photos")
-os.chdir("jpg_photos")
-os.system("cd")
-prediction = model.predict([prepare('8.jpg')])      #always pass a list
-#print(prediction)   # Will be a list in a list
-print(CATEGORIES[int(prediction[0][0])])
-
-#classes = np.argmax(prediction, axis = 1)
-#print(classes)        
-
-
-model = tf.keras.models.load_model('new_model.h5')
-model.compile(loss="sparse_categorical_crossentropy", optimizer= "adam", metrics=['accuracy'])
-IMG_SIZE = 28
-img = image.load_img(image_path, (IMG_SIZE,IMG_SIZE))
-img = cv2.resize(img, (IMG_SIZE, IMG_SIZE))
-plt.imshow(img)
-img = np.expand_dims(img, axis=0)
-prediction = model.predict(img)
-print(CATEGORIES[int(prediction[0][0])])
-
-
-'''
